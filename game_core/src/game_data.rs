c3d247c5b5c11ee995a0ef1e18aa472efcc782e2
Artewald <matras.tim@gmail.com>
2023-04-30 20:51:51 +0200

    Fix clippy error

--- game_core/src/game_data.rs
+++ game_core/src/game_data.rs
@@ -733,14 +733,14 @@ impl SituationCardList {
 
     pub fn get_default_situation_card_by_id(id: SituationCardID) -> Result<SituationCard, String> {
         let situation_cards = crate::situation_card_list::situation_card_list_wrapper();
-        match situation_cards
+        situation_cards
             .situation_cards
             .iter()
             .find(|card| card.card_id == id)
-        {
-            Some(card) => Ok(card.clone()),
-            None => Err(format!("There was no code with the ID: {}", id)),
-        }
+            .map_or_else(
+                || Err(format!("There was no code with the ID: {}", id)),
+                |card| Ok(card.clone()),
+            )
     }
 }
 
